22614970fd3e600de51d3dca0f57cd458d289092
evasyukov <egor.vasyukov1@gmail.com>
2024-03-01 14:53:11 +0700

    create and add component quatrain witch verse

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
-<script setup></script>
+<script setup>
+import Quatrain from './components/Quatrain.vue';
+
+
+</script>
 
 <template>
   <div class="title">
     <h1 class="verse">Invictus</h1>
     <h3 class="autor">by William Ernest Henley</h3>
   </div>
+  <Quatrain></Quatrain>
 </template>
 
 <style scoped>
