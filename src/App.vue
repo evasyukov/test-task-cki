6ae9ad4f7d656b2692228be36c54c59d18f83146
evasyukov <egor.vasyukov1@gmail.com>
2024-03-01 14:24:07 +0700

    initial layout and styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-<h1>hello</h1>
-
+  <div class="title">
+    <h1 class="verse">Invictus</h1>
+    <h3 class="autor">by William Ernest Henley</h3>
+  </div>
 </template>
 
 <style scoped>
+.title {
+  display: flex;
+  justify-content: center;
+}
 
+.autor {
+  padding-left: 5vh;
+}
 </style>
