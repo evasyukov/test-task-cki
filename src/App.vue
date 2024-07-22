e41293e889061218e94e3bb009192d8471123f41
evasyukov <egor.vasyukov1@gmail.com>
2024-07-22 14:10:00 +0700

    add button for change theme

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,10 @@
     />
     <Quatrain :visibleStanzas="visibleStanzas" />
   </div>
+
+  <div class="change-theme">
+    <button @click="changeTheme">Сменить тему</button>
+  </div>
 </template>
 
 <script>
@@ -30,6 +34,17 @@ export default {
     toggleStanzaVisibility(index) {
       this.visibleStanzas[index] = !this.visibleStanzas[index]
     },
+    changeTheme() {
+      const bodyClass = document.querySelector("body")
+
+      if (bodyClass.className.includes("dark-theme")) {
+        bodyClass.classList.remove("dark-theme")
+        bodyClass.classList.add("light-theme")
+      } else {
+        bodyClass.classList.remove("light-theme")
+        bodyClass.classList.add("dark-theme")
+      }
+    },
   },
 }
 </script>
@@ -47,6 +62,14 @@ export default {
   display: flex;
   justify-content: center;
 }
-
-
+.change-theme button{
+  background-color: transparent;
+  color: #1206f6;
+  border: 1px solid #1206f6; 
+  padding: 10px 15px; 
+  cursor: pointer; 
+  position: absolute;
+  top: 30px;
+  left: 270px;
+}
 </style>
